51db159165cd934e1a4502b9bf01104bbc1ada3d
Radim Hejduk <dev@radimhejduk.cz>
2022-12-08 17:40:35 +0100

    unsplash API

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,10 @@ use chrono::prelude::*;
 
 fn main() {
     let next_year: i32 = Utc::now().year() + 1;
-    print!("{}", next_year);
+    let mut unsplash_api = String::from("https://api.unsplash.com/search/photos?client_id=JRzZBQwAW24--mdyrlbWwJfIxcuxfsCSlCQNKSDeN4E&query=");
+    // add option to change query string
+    unsplash_api.push_str("christmas");
+
+    println!("{}", next_year);
+    println!("{}", unsplash_api);
 }
