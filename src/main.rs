bac1f68a0c51e9c1c69536684cfdd747171d45f8
Radim Hejduk <dev@radimhejduk.cz>
2022-12-10 15:40:22 +0100

    accessing data from unsplash

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 use chrono::prelude::*;
+use unsplash_api::endpoints::search_photos::SearchPhotos;
+use http_api_client_endpoint::Endpoint;
+use serde_json::Value;
 
 fn main() {
     let next_year: i32 = Utc::now().year() + 1;
-    let mut unsplash_api = String::from("https://api.unsplash.com/search/photos?client_id=JRzZBQwAW24--mdyrlbWwJfIxcuxfsCSlCQNKSDeN4E&query=");
-    // add option to change query string
-    unsplash_api.push_str("christmas");
+    let unsplash_uri = SearchPhotos::new("JRzZBQwAW24--mdyrlbWwJfIxcuxfsCSlCQNKSDeN4E", "christmas")
+        .render_request()
+        .unwrap()
+        .uri()
+        .to_string()
+    ;
+    let body = reqwest::blocking::get(unsplash_uri).unwrap().text();
+    let deserialized: Value = serde_json::from_str(&body.unwrap()).unwrap();
+    let results = deserialized.get("results").unwrap();
+
+    if results.is_array() {
+        for item in results.as_array().unwrap() {
+            //TODO: get regular url, download image, save it
+            dbg!(item.get("urls"));
+        }
+    }
 
     println!("{}", next_year);
-    println!("{}", unsplash_api);
 }
