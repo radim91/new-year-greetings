97912b0ff63ce9c7cde3cc92df69f109b7099868
Radim Hejduk <dev@radimhejduk.cz>
2022-12-11 11:20:09 +0100

    downloading image

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use chrono::prelude::*;
 use unsplash_api::endpoints::search_photos::SearchPhotos;
 use http_api_client_endpoint::Endpoint;
 use serde_json::Value;
+use image::ImageFormat;
+use random_string::generate;
 
 fn main() {
     let next_year: i32 = Utc::now().year() + 1;
@@ -11,14 +13,25 @@ fn main() {
         .uri()
         .to_string()
     ;
-    let body = reqwest::blocking::get(unsplash_uri).unwrap().text();
-    let deserialized: Value = serde_json::from_str(&body.unwrap()).unwrap();
+    let body = reqwest::blocking::get(unsplash_uri).unwrap().text().unwrap();
+    let deserialized: Value = serde_json::from_str(&body).unwrap();
     let results = deserialized.get("results").unwrap();
 
     if results.is_array() {
         for item in results.as_array().unwrap() {
-            //TODO: get regular url, download image, save it
-            dbg!(item.get("urls"));
+            let mut link = item.get("urls").unwrap().get("regular").unwrap().to_string();
+
+            if link.len() > 0 {
+                link.remove(0);
+                link.pop();
+            }
+
+            let res = reqwest::blocking::get(&link).unwrap().bytes();
+            let downloaded_image = image::load_from_memory(&res.unwrap()).unwrap();
+            let random_string = generate(10, "abcdefghijklmnopqrstuvwxyz1234567890");
+            let path = format!("./downloads/image-{}.png", random_string);
+
+            downloaded_image.save_with_format(path, ImageFormat::Png);
         }
     }
 
